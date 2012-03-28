908e0ff2f5705fa73bbe74a24914c2f316f5f5fb
Arnout Kazemier <info@3rd-Eden.com>
2012-03-28 17:15:35 +0200

    Retab, I hate mixed content, sorry..
    Also I fixed double click zoom issues in IE & Opera, the
    wrapping container didn't cancel these events so they where actually causing the map to zoom in at the location of the dblclick.

--- src/control/Control.Zoom.js
+++ src/control/Control.Zoom.js
@@ -1,39 +1,43 @@
 L.Control.Zoom = L.Control.extend({
-	options: {
-		position: 'topleft'
-	},
+  options: {
+    position: 'topleft'
+  },
 
-	onAdd: function (map) {
-		var className = 'leaflet-control-zoom',
-		    container = L.DomUtil.create('div', className);
+  onAdd: function (map) {
+    var className = 'leaflet-control-zoom',
+        container = L.DomUtil.create('div', className);
 
-		this._createButton('Zoom in', className + '-in', container, map.zoomIn, map);
-		this._createButton('Zoom out', className + '-out', container, map.zoomOut, map);
+    this._createButton('Zoom in', className + '-in', container, map.zoomIn, map);
+    this._createButton('Zoom out', className + '-out', container, map.zoomOut, map);
 
-		return container;
-	},
+    L.DomEvent
+      .addListener(container, 'dblclick', L.DomEvent.stopPropagation)
+      .addListener(container, 'dblclick', L.DomEvent.preventDefault);
 
-	_createButton: function (title, className, container, fn, context) {
-		var link = L.DomUtil.create('a', className, container);
-		link.href = '#';
-		link.title = title;
+    return container;
+  },
 
-		L.DomEvent
-			.addListener(link, 'click', L.DomEvent.stopPropagation)
-			.addListener(link, 'click', L.DomEvent.preventDefault)
-			.addListener(link, 'click', fn, context);
+  _createButton: function (title, className, container, fn, context) {
+    var link = L.DomUtil.create('a', className, container);
+    link.href = '#';
+    link.title = title;
 
-		return link;
-	}
+    L.DomEvent
+      .addListener(link, 'click', L.DomEvent.stopPropagation)
+      .addListener(link, 'click', L.DomEvent.preventDefault)
+      .addListener(link, 'click', fn, context);
+
+    return link;
+  }
 });
 
 L.Map.mergeOptions({
-	zoomControl: true
+  zoomControl: true
 });
 
 L.Map.addInitHook(function () {
-	if (this.options.zoomControl) {
-		this.zoomControl = new L.Control.Zoom();
-		this.addControl(this.zoomControl);
-	}
+  if (this.options.zoomControl) {
+    this.zoomControl = new L.Control.Zoom();
+    this.addControl(this.zoomControl);
+  }
 });
